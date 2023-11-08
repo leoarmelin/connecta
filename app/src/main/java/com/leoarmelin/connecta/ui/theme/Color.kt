51ebeb6ad78163b6a998a620f0936d9454992ed3
felipevieira <felipe2204sv@gmail.com>
2023-11-08 14:29:14 -0300

    theme: apply custom theme

--- app/src/main/java/com/leoarmelin/connecta/ui/theme/Color.kt
+++ app/src/main/java/com/leoarmelin/connecta/ui/theme/Color.kt
@@ -1,11 +1,89 @@
 package com.leoarmelin.connecta.ui.theme
-
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val md_theme_light_primary = Color(0xFF764B9C)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_primaryContainer = Color(0xFFF1DAFF)
+val md_theme_light_onPrimaryContainer = Color(0xFF2D004F)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val md_theme_light_secondary = Color(0xFF874589)
+val md_theme_light_onSecondary = Color(0xFFFFFFFF)
+val md_theme_light_secondaryContainer = Color(0xFFFFD6FA)
+val md_theme_light_onSecondaryContainer = Color(0xFF37003C)
+
+val md_theme_light_tertiary = Color(0xFF5256A9)
+val md_theme_light_onTertiary = Color(0xFFFFFFFF)
+val md_theme_light_tertiaryContainer = Color(0xFFE1E0FF)
+val md_theme_light_onTertiaryContainer = Color(0xFF080764)
+
+val md_theme_light_error = Color(0xFFBA1A1A)
+val md_theme_light_errorContainer = Color(0xFFFFDAD6)
+val md_theme_light_onError = Color(0xFFFFFFFF)
+val md_theme_light_onErrorContainer = Color(0xFF410002)
+
+val md_theme_light_background = Color(0xFFFDFBFF)
+val md_theme_light_onBackground = Color(0xFF001B3D)
+
+val md_theme_light_surface = Color(0xFFFDFBFF)
+val md_theme_light_onSurface = Color(0xFF001B3D)
+val md_theme_light_surfaceVariant = Color(0xFFE9DFEA)
+val md_theme_light_onSurfaceVariant = Color(0xFF4B454D)
+val md_theme_light_inverseSurface = Color(0xFF003062)
+val md_theme_light_inverseOnSurface = Color(0xFFECF0FF)
+
+val md_theme_light_outline = Color(0xFF7C757E)
+val md_theme_light_inversePrimary = Color(0xFFDFB7FF)
+val md_theme_light_shadow = Color(0xFF000000)
+val md_theme_light_surfaceTint = Color(0xFF764B9C)
+val md_theme_light_outlineVariant = Color(0xFFCDC4CE)
+val md_theme_light_scrim = Color(0xFF000000)
+
+/** Dark Mode */
+val md_theme_dark_primary = Color(0xFFDFB7FF)
+val md_theme_dark_onPrimary = Color(0xFF451969)
+val md_theme_dark_primaryContainer = Color(0xFF5D3282)
+val md_theme_dark_onPrimaryContainer = Color(0xFFF1DAFF)
+
+val md_theme_dark_secondary = Color(0xFFFBACF8)
+val md_theme_dark_onSecondary = Color(0xFF521457)
+val md_theme_dark_secondaryContainer = Color(0xFF6C2D70)
+val md_theme_dark_onSecondaryContainer = Color(0xFFFFD6FA)
+
+val md_theme_dark_tertiary = Color(0xFFC0C1FF)
+val md_theme_dark_onTertiary = Color(0xFF222578)
+val md_theme_dark_tertiaryContainer = Color(0xFF3A3D8F)
+val md_theme_dark_onTertiaryContainer = Color(0xFFE1E0FF)
+
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = Color(0xFF690005)
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+
+val md_theme_dark_background = Color(0xFF001B3D)
+val md_theme_dark_onBackground = Color(0xFFD6E3FF)
+
+val md_theme_dark_surface = Color(0xFF001B3D)
+val md_theme_dark_onSurface = Color(0xFFD6E3FF)
+val md_theme_dark_surfaceVariant = Color(0xFF4B454D)
+val md_theme_dark_onSurfaceVariant = Color(0xFFCDC4CE)
+val md_theme_dark_inverseOnSurface = Color(0xFF001B3D)
+val md_theme_dark_inverseSurface = Color(0xFFD6E3FF)
+
+val md_theme_dark_outline = Color(0xFF968E98)
+val md_theme_dark_inversePrimary = Color(0xFF764B9C)
+val md_theme_dark_shadow = Color(0xFF000000)
+val md_theme_dark_surfaceTint = Color(0xFFDFB7FF)
+val md_theme_dark_outlineVariant = Color(0xFF4B454D)
+val md_theme_dark_scrim = Color(0xFF000000)
+
+
+val seed = Color(0xFFCB9CF2)
+val Quaternary = Color(0xFF616283)
+val light_Quaternary = Color(0xFF5256A9)
+val light_onQuaternary = Color(0xFFFFFFFF)
+val light_QuaternaryContainer = Color(0xFFE1E0FF)
+val light_onQuaternaryContainer = Color(0xFF080764)
+val dark_Quaternary = Color(0xFFC0C1FF)
+val dark_onQuaternary = Color(0xFF222578)
+val dark_QuaternaryContainer = Color(0xFF3A3D8F)
+val dark_onQuaternaryContainer = Color(0xFFE1E0FF)
