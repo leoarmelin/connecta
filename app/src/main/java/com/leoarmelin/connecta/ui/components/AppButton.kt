1265a8e064b819c0174b2bcab16db74932e5d30b
felipevieira <felipe2204sv@gmail.com>
2023-11-14 16:51:25 -0300

    improve buttons

--- app/src/main/java/com/leoarmelin/connecta/ui/components/AppButton.kt
+++ app/src/main/java/com/leoarmelin/connecta/ui/components/AppButton.kt
@@ -1,13 +1,23 @@
 package com.leoarmelin.connecta.ui.components
 
 import android.annotation.SuppressLint
+import android.view.MotionEvent
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.input.pointer.pointerInteropFilter
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.leoarmelin.connecta.ui.theme.ConnectaTheme
@@ -15,6 +25,7 @@ import com.leoarmelin.connecta.ui.theme.Typography
 import androidx.compose.material3.MaterialTheme.colorScheme as mtc
 
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 @SuppressLint("ModifierParameter")
 fun AppButton(
@@ -22,8 +33,14 @@ fun AppButton(
     modifier: Modifier = Modifier,
     onClick: () -> Unit,
 ) {
+    var isPressed by remember { mutableStateOf(false) }
+    val scale by animateFloatAsState(
+        targetValue = if (isPressed) 1.1f else 1.0f,
+        label = "scale"
+    )
+
     Button(
-        onClick = onClick,
+        onClick = {},
         shape = RoundedCornerShape(16.dp),
         colors = ButtonDefaults.buttonColors(
             containerColor = mtc.primaryContainer,
@@ -31,7 +48,26 @@ fun AppButton(
             disabledContainerColor = mtc.primaryContainer.copy(alpha = 0.5f),
             disabledContentColor = mtc.onPrimaryContainer.copy(alpha = 0.5f),
         ),
+        border = BorderStroke(2.dp, mtc.onPrimaryContainer),
         modifier = modifier
+            .pointerInteropFilter {
+                when (it.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        isPressed = true
+                    }
+
+                    MotionEvent.ACTION_UP -> {
+                        onClick()
+                        isPressed = false
+                    }
+
+                    MotionEvent.ACTION_CANCEL -> {
+                        isPressed = false
+                    }
+                }
+                true
+            }
+            .scale(scale)
     ) {
         Text(
             text = text,
